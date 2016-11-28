325c5f2a2509c12aebdd838231111d7a4bae8a2f
Charles Clemens <cclemensjr@gmail.com>
2016-11-28 04:46:57 -0800

    added a few gems

--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -6,4 +6,13 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "#{@tag.name} Tag Destroyed!"
+
+    redirect_to tags_path(@tag)
+  end
 end
